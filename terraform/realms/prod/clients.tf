847acaf3a1ff196a2e3d8f481e982a6f77c31c25
Jamie <46281327+jamieernest@users.noreply.github.com>
2026-09-22 16:35:48 +0100

    fix: format

--- terraform/realms/prod/clients.tf
+++ terraform/realms/prod/clients.tf
@@ -146,9 +146,9 @@ module "grafana_client" {
 module "dress_notes_client" {
   source = "../../modules/oidc-client"
 
-  realm           = module.realm
-  client_id       = "dress_notes"
-  name            = "Dress Notes"
+  realm = module.realm
+  client_id = "dress_notes"
+  name = "Dress Notes"
 
   enabled_flows = {
     authorization_code = true
